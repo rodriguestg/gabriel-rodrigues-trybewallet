9cb9a30a6707f6b519b5bf7db993a34e1fd6be1c
rodriguestg <grs.ibr@gmail.com>
2022-07-28 20:21:14 -0300

    Subindo requisito 1

--- src/redux/actions/index.js
+++ src/redux/actions/index.js
@@ -1,1 +1,6 @@
 // Coloque aqui suas actions
+// import { USER_LOGIN } from '../actions/actionTypes';
+const USER_LOGIN = 'LOGIN';
+export const loginUser = (state) => ({ type: USER_LOGIN, state });
+const USER_1 = '1';
+export const user = (state) => ({ type: USER_1, state });
