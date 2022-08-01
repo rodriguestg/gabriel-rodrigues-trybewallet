409484c547bf050be77c0ae8a08cce8a7ec525fb
rodriguestg <grs.ibr@gmail.com>
2022-08-01 17:03:54 -0300

    Subindo requisito 3

--- src/components/WalletForm.js
+++ src/components/WalletForm.js
@@ -1,9 +1,63 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
+import { fetchMoney } from '../redux/actions';
 
-export default class WalletForm extends Component {
+class WalletForm extends Component {
+  componentDidMount() {
+    const { fetchMoneyDispatch } = this.props;
+    fetchMoneyDispatch();
+  }
+
   render() {
+    const { currencies } = this.props;
+    console.log(currencies);
+    currencies.splice(1, 1);
     return (
-      <div>WalletForm</div>
+      <div>
+        <form>
+          <input
+            data-testid="value-input"
+            name="value"
+            type="number"
+          />
+          <input
+            data-testid="description-input"
+            name="description"
+            type="text"
+          />
+          <select data-testid="currency-input" name="currency">
+            {
+              currencies.map((coin) => <option key={ coin }>{ coin }</option>)
+            }
+          </select>
+          <select data-testid="method-input" name="method">
+            <option value="dinheiro">Dinheiro</option>
+            <option value="crédito">Cartão de crédito</option>
+            <option value="débito">Cartão de débito</option>
+          </select>
+          <select data-testid="tag-input" name="tag">
+            <option value="Alimentação">Alimentação</option>
+            <option value="Lazer">Lazer</option>
+            <option value="Trabalho">Trabalho</option>
+            <option value="Transporte">Transporte</option>
+            <option value="Saúde">Saúde</option>
+          </select>
+        </form>
+      </div>
     );
   }
 }
+
+const mapStateToProps = (state) => ({
+  currencies: state.wallet.currencies });
+
+const mapDispatchToProps = (dispatch) => ({
+  fetchMoneyDispatch: () => dispatch(fetchMoney()),
+});
+
+WalletForm.propTypes = {
+  currencies: PropTypes.arrayOf(PropTypes.string).isRequired,
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(WalletForm);
