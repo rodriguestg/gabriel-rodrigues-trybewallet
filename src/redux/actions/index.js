2db65383ba49c9aaddd3a6e147388e7628b8c8e4
rodriguestg <grs.ibr@gmail.com>
2022-08-02 17:35:04 -0300

    Ajustes de texto

--- src/redux/actions/index.js
+++ src/redux/actions/index.js
@@ -1,5 +1,4 @@
 // Coloque aqui suas actions
-// import { USER_LOGIN } from '../actions/actionTypes';
 const USER_LOGIN = 'LOGIN';
 export const loginUser = (state) => ({ type: USER_LOGIN, state });
 
@@ -7,7 +6,6 @@ const ENDPOINT = 'https://economia.awesomeapi.com.br/json/all';
 const USER_COINS = 'COINS';
 const coins = (state) => ({ type: USER_COINS, state });
 export function fetchMoney() {
-  // console.log('oi');
   return async (dispatch) => {
     const response = await fetch(ENDPOINT);
     const data = await response.json();
@@ -18,7 +16,6 @@ export function fetchMoney() {
 const USER_DESPESA = 'COINS_DESPESA';
 const despesa = (state, stateLocal) => ({ type: USER_DESPESA, state, stateLocal });
 export function fetchDespesas(stateLocal) {
-  // console.log('oi');
   // console.log(stateLocal);
   return async (dispatch) => {
     const response = await fetch(ENDPOINT);
