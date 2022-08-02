b7822324c47cbb16a31521e760402309be5d5fce
rodriguestg <grs.ibr@gmail.com>
2022-08-02 08:12:41 -0300

    Subindo requisito 4

--- src/components/WalletForm.js
+++ src/components/WalletForm.js
@@ -1,48 +1,90 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
-import { fetchMoney } from '../redux/actions';
+import { fetchDespesas, fetchMoney } from '../redux/actions';
 
 class WalletForm extends Component {
+  constructor() {
+    super();
+    this.state = {
+      number: 0,
+      description: '',
+      currency: 'USD',
+      method: 'Dinheiro',
+      tag: 'Alimentação',
+    };
+  }
+
   componentDidMount() {
     const { fetchMoneyDispatch } = this.props;
     fetchMoneyDispatch();
   }
 
+  inputState = ({ target }) => {
+    // console.log(target);
+    const { name, value } = target;
+    this.setState({
+      [name]: value,
+    });
+  }
+
+  add = () => {
+    const { fetchDespesasDispatch } = this.props;
+    fetchDespesasDispatch(this.state);
+    this.setState({
+      number: 0,
+      description: '',
+      currency: 'USD',
+      method: 'Dinheiro',
+      tag: '',
+    });
+  }
+
   render() {
     const { currencies } = this.props;
-    console.log(currencies);
-    currencies.splice(1, 1);
+    const { tag } = this.state;
+    const currenciesFilter = currencies.filter((currencie) => currencie !== 'USDT');
     return (
       <div>
         <form>
           <input
             data-testid="value-input"
-            name="value"
+            name="number"
+            id="1"
             type="number"
+            onChange={ this.inputState }
           />
           <input
             data-testid="description-input"
             name="description"
+            id="2"
             type="text"
+            onChange={ this.inputState }
           />
-          <select data-testid="currency-input" name="currency">
+          <select
+            data-testid="currency-input"
+            name="currency"
+            onChange={ this.inputState }
+          >
             {
-              currencies.map((coin) => <option key={ coin }>{ coin }</option>)
+              currenciesFilter.map((coin) => <option key={ coin }>{ coin }</option>)
             }
           </select>
-          <select data-testid="method-input" name="method">
-            <option value="dinheiro">Dinheiro</option>
-            <option value="crédito">Cartão de crédito</option>
-            <option value="débito">Cartão de débito</option>
+          <select data-testid="method-input" name="method" onChange={ this.inputState }>
+            <option value="Dinheiro">Dinheiro</option>
+            <option value="Cartão de crédito">Cartão de crédito</option>
+            <option value="Cartão de débito">Cartão de débito</option>
           </select>
-          <select data-testid="tag-input" name="tag">
-            <option value="Alimentação">Alimentação</option>
+          <select data-testid="tag-input" name="tag" onChange={ this.inputState }>
+            <option value={ tag }>Alimentação</option>
             <option value="Lazer">Lazer</option>
             <option value="Trabalho">Trabalho</option>
             <option value="Transporte">Transporte</option>
             <option value="Saúde">Saúde</option>
           </select>
+          <button type="reset" onClick={ this.add }>
+            Adicionar despesa
+          </button>
         </form>
       </div>
     );
@@ -54,11 +96,13 @@ const mapStateToProps = (state) => ({
 
 const mapDispatchToProps = (dispatch) => ({
   fetchMoneyDispatch: () => dispatch(fetchMoney()),
+  fetchDespesasDispatch: (state) => dispatch(fetchDespesas(state)),
 });
 
 WalletForm.propTypes = {
   currencies: PropTypes.arrayOf(PropTypes.string).isRequired,
   fetchMoneyDispatch: PropTypes.func.isRequired,
+  fetchDespesasDispatch: PropTypes.func.isRequired,
 };
 
 export default connect(mapStateToProps, mapDispatchToProps)(WalletForm);
--- src/redux/actions/index.js
+++ src/redux/actions/index.js
@@ -3,8 +3,8 @@
 const USER_LOGIN = 'LOGIN';
 export const loginUser = (state) => ({ type: USER_LOGIN, state });
 
-const USER_COINS = 'COINS';
 const ENDPOINT = 'https://economia.awesomeapi.com.br/json/all';
+const USER_COINS = 'COINS';
 const coins = (state) => ({ type: USER_COINS, state });
 export function fetchMoney() {
   // console.log('oi');
@@ -12,5 +12,17 @@ export function fetchMoney() {
     const response = await fetch(ENDPOINT);
     const data = await response.json();
     dispatch(coins(data));
+  };
+}
+
+const USER_DESPESA = 'COINS_DESPESA';
+const despesa = (state, stateLocal) => ({ type: USER_DESPESA, state, stateLocal });
+export function fetchDespesas(stateLocal) {
+  // console.log('oi');
+  // console.log(stateLocal);
+  return async (dispatch) => {
+    const response = await fetch(ENDPOINT);
+    const data = await response.json();
+    dispatch(despesa(data, stateLocal));
   };
 }
